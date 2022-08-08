00f07880c6827ade2fae09f78cb8042709850f35
JP <johnpaulmitra@yahoo.com>
2022-08-08 01:51:57 -0500

    heroku commit

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ module.exports = (passport) => {
   passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://mood-tracker-demo.herokuapp.com/auth/google/callback"
+    callbackURL: "http://localhost:3000/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     const newUser = {
