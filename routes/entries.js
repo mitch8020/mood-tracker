c2deb162d5fce62f4c96b8da7a6eef4faf4cd110
JP <johnpaulmitra@yahoo.com>
2022-08-09 17:12:26 -0500

    Added PUT & DELETE functionality to entries

--- routes/entries.js
+++ routes/entries.js
@@ -41,106 +41,106 @@ router.get('/', ensureAuth, async (req, res) => {
   }
 })
 
-// // @desc    Show Single
-// // @route   GET /stories/:id
-// router.get('/:id', ensureAuth, async (req, res) => {
-//   try {
-//     let story = await Story.findById(req.params.id)
-//       .populate('user')
-//       .lean()
+// @desc    Show Single Entry
+// @route   GET /entries/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+  try {
+    let entry = await Entry.findById(req.params.id)
+      .populate('user')
+      .lean()
 
-//     res.render('stories/show', {
-//       story
-//     })
-//   } catch (err) {
-//     console.err(err)
-//     return res.render('error/404')
-//   }
-// })
+    res.render('entries/show', {
+      entry
+    })
+  } catch (err) {
+    console.error(err)
+    return res.render('error/404')
+  }
+})
 
-// // @desc    Show Edit Page
-// // @route   GET /stories/edit/:id
-// router.get('/edit/:id', ensureAuth, async (req, res) => {
-//   try {
-//     const story = await Story.findOne({
-//       _id: req.params.id
-//     }).lean()
+// @desc    Show Edit Page
+// @route   GET /entries/edit/:id
+router.get('/edit/:id', ensureAuth, async (req, res) => {
+  try {
+    const entry = await Entry.findOne({
+      _id: req.params.id
+    }).lean()
   
-//     if (!story) {
-//       return res.render('error/404')
-//     }
+    if (!entry) {
+      return res.render('error/404')
+    }
   
-//     if (story.user != req.user.id) {
-//       res.redirect('/stories')
-//     } else {
-//       res.render('stories/edit', {
-//         story
-//       })
-//     }
-//   } catch (err) {
-//     console.error(err)
-//     return res.render('error/500')
-//   }
+    if (entry.user != req.user.id) {
+      res.redirect('/entries')
+    } else {
+      res.render('entries/edit', {
+        entry
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
+  }
   
-// })
+})
 
-// // @desc    Update Story
-// // @route   PUT /stories/:id
-// router.put('/:id', ensureAuth, async (req, res) => {
-//   try {
-//     let story = await Story.findById(req.params.id).lean()
+// @desc    Update Entry
+// @route   PUT /stories/:id
+router.put('/:id', ensureAuth, async (req, res) => {
+  try {
+    let entry = await Entry.findById(req.params.id).lean()
 
-//     if (!story) {
-//       return res.render('error/404')
-//     }
+    if (!entry) {
+      return res.render('error/404')
+    }
   
-//     if (story.user != req.user.id) {
-//       res.redirect('/stories')
-//     } else {
-//       story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
-//         new: true,
-//         runValidators: true
-//       })
+    if (entry.user != req.user.id) {
+      res.redirect('/entries')
+    } else {
+      entry = await Entry.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true
+      })
   
-//       res.redirect('/dashboard')
-//     }
-//   } catch (err) {
-//     console.error(err)
-//     return res.render('error/500')
-//   }
+      res.redirect('/dashboard')
+    }
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
+  }
   
-// })
+})
 
-// // @desc    Delete Story
-// // @route   DELETE /stories/:id
-// router.delete('/:id', ensureAuth, async (req, res) => {
-//   try {
-//     await Story.remove({ _id: req.params.id })
-//     res.redirect('/dashboard')
-//   } catch (err) {
-//     console.error(err)
-//     return res.render('error/500')
-//   }
-// })
+// @desc    Delete Entry
+// @route   DELETE /entries/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+    await Entry.remove({ _id: req.params.id })
+    res.redirect('/dashboard')
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
+  }
+})
 
-// // @desc    User Stories
-// // @route   GET /stories/user/:userId
-// router.get('/user/:userId', ensureAuth, async (req, res) => {
-//   try {
-//     const stories = await Story.find({
-//       user: req.params.userId,
-//       status: 'public'
-//     })
-//     .populate('user')
-//     .lean()
+// @desc    User Entries
+// @route   GET /entries/user/:userId
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+  try {
+    const entries = await Entry.find({
+      user: req.params.userId
+    })
+    .populate('user')
+    .lean()
 
-//     res.render('stories/index', {
-//       stories
-//     })
-//   } catch (err) {
-//     console.error(err)
-//     return res.render('error/500')
-//   }
-// })
+    res.render('entries/user', {
+      name: req.user.firstName,
+      entries
+    })
+  } catch (err) {
+    console.error(err)
+    return res.render('error/404')
+  }
+})
 
 module.exports = router
